66abc8ac87402a13107ed4e4cd404549728e4419
Tanner Olsen <tanner-olsen@hotmail.com>
2023-02-11 20:14:35 -0500

    Removed animations. Not working!

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -28,9 +28,9 @@ namespace StarterAssets
         [Tooltip("Acceleration and deceleration")]
         public float SpeedChangeRate = 10.0f;
 
-        //public AudioClip LandingAudioClip;
-        //public AudioClip[] FootstepAudioClips;
-        //[Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+        public AudioClip LandingAudioClip;
+        public AudioClip[] FootstepAudioClips;
+        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
 
         [Space(10)]
         [Tooltip("The height the player can jump")]
@@ -81,7 +81,7 @@ namespace StarterAssets
 
         // player
         private float _speed;
-        //private float _animationBlend;
+        private float _animationBlend;
         private float _targetRotation = 0.0f;
         private float _rotationVelocity;
         private float _verticalVelocity;
@@ -92,16 +92,16 @@ namespace StarterAssets
         private float _fallTimeoutDelta;
 
         // animation IDs
-        //private int _animIDSpeed;
-        //private int _animIDGrounded;
-        //private int _animIDJump;
-        //private int _animIDFreeFall;
-        //private int _animIDMotionSpeed;
+        private int _animIDSpeed;
+        private int _animIDGrounded;
+        private int _animIDJump;
+        private int _animIDFreeFall;
+        private int _animIDMotionSpeed;
 
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
         private PlayerInput _playerInput;
 #endif
-        //private Animator _animator;
+        private Animator _animator;
         private CharacterController _controller;
         private StarterAssetsInputs _input;
         private GameObject _mainCamera;
@@ -135,8 +135,8 @@ namespace StarterAssets
         private void Start()
         {
             _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
-            
-            //_hasAnimator = TryGetComponent(out _animator);
+
+            _hasAnimator = TryGetComponent(out _animator);
             _controller = GetComponent<CharacterController>();
             _input = GetComponent<StarterAssetsInputs>();
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
@@ -145,7 +145,7 @@ namespace StarterAssets
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
 
-            //AssignAnimationIDs();
+            AssignAnimationIDs();
 
             // reset our timeouts on start
             _jumpTimeoutDelta = JumpTimeout;
@@ -154,7 +154,7 @@ namespace StarterAssets
 
         private void Update()
         {
-            //_hasAnimator = TryGetComponent(out _animator);
+            _hasAnimator = TryGetComponent(out _animator);
 
             JumpAndGravity();
             GroundedCheck();
@@ -166,14 +166,14 @@ namespace StarterAssets
             CameraRotation();
         }
 
-        //private void AssignAnimationIDs()
-        //{
-        //    _animIDSpeed = Animator.StringToHash("Speed");
-        //    _animIDGrounded = Animator.StringToHash("Grounded");
-        //    _animIDJump = Animator.StringToHash("Jump");
-        //    _animIDFreeFall = Animator.StringToHash("FreeFall");
-        //    _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
-        //}
+        private void AssignAnimationIDs()
+        {
+            _animIDSpeed = Animator.StringToHash("Speed");
+            _animIDGrounded = Animator.StringToHash("Grounded");
+            _animIDJump = Animator.StringToHash("Jump");
+            _animIDFreeFall = Animator.StringToHash("FreeFall");
+            _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+        }
 
         private void GroundedCheck()
         {
@@ -184,10 +184,10 @@ namespace StarterAssets
                 QueryTriggerInteraction.Ignore);
 
             // update animator if using character
-            //if (_hasAnimator)
-            //{
-            //    _animator.SetBool(_animIDGrounded, Grounded);
-            //}
+            if (_hasAnimator)
+            {
+                _animator.SetBool(_animIDGrounded, Grounded);
+            }
         }
 
         private void CameraRotation()
@@ -245,8 +245,8 @@ namespace StarterAssets
                 _speed = targetSpeed;
             }
 
-            //_animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
-            //if (_animationBlend < 0.01f) _animationBlend = 0f;
+            _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+            if (_animationBlend < 0.01f) _animationBlend = 0f;
 
             // normalise input direction
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
@@ -272,11 +272,11 @@ namespace StarterAssets
                              new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 
             // update animator if using character
-            //if (_hasAnimator)
-            //{
-            //    _animator.SetFloat(_animIDSpeed, _animationBlend);
-            //    _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
-            //}
+            if (_hasAnimator)
+            {
+                _animator.SetFloat(_animIDSpeed, _animationBlend);
+                _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+            }
         }
 
         private void JumpAndGravity()
@@ -286,12 +286,12 @@ namespace StarterAssets
                 // reset the fall timeout timer
                 _fallTimeoutDelta = FallTimeout;
 
-                // update animator if using character
-                //if (_hasAnimator)
-                //{
-                //    _animator.SetBool(_animIDJump, false);
-                //    _animator.SetBool(_animIDFreeFall, false);
-                //}
+                //update animator if using character
+                if (_hasAnimator)
+                {
+                    _animator.SetBool(_animIDJump, false);
+                    _animator.SetBool(_animIDFreeFall, false);
+                }
 
                 // stop our velocity dropping infinitely when grounded
                 if (_verticalVelocity < 0.0f)
@@ -305,11 +305,11 @@ namespace StarterAssets
                     // the square root of H * -2 * G = how much velocity needed to reach desired height
                     _verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
 
-                    //// update animator if using character
-                    //if (_hasAnimator)
-                    //{
-                    //    _animator.SetBool(_animIDJump, true);
-                    //}
+                    // update animator if using character
+                    if (_hasAnimator)
+                    {
+                        _animator.SetBool(_animIDJump, true);
+                    }
                 }
 
                 // jump timeout
@@ -331,10 +331,10 @@ namespace StarterAssets
                 else
                 {
                     //// update animator if using character
-                    //if (_hasAnimator)
-                    //{
-                    //    _animator.SetBool(_animIDFreeFall, true);
-                    //}
+                    if (_hasAnimator)
+                    {
+                        _animator.SetBool(_animIDFreeFall, true);
+                    }
                 }
 
                 // if we are not grounded, do not jump
@@ -369,24 +369,24 @@ namespace StarterAssets
                 GroundedRadius);
         }
 
-        //private void OnFootstep(AnimationEvent animationEvent)
-        //{
-        //    if (animationEvent.animatorClipInfo.weight > 0.5f)
-        //    {
-        //        if (FootstepAudioClips.Length > 0)
-        //        {
-        //            var index = Random.Range(0, FootstepAudioClips.Length);
-        //            AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
-        //        }
-        //    }
-        //}
+        private void OnFootstep(AnimationEvent animationEvent)
+        {
+            if (animationEvent.animatorClipInfo.weight > 0.5f)
+            {
+                if (FootstepAudioClips.Length > 0)
+                {
+                    var index = Random.Range(0, FootstepAudioClips.Length);
+                    AudioSource.PlayClipAtPoint(FootstepAudioClips[index], transform.TransformPoint(_controller.center), FootstepAudioVolume);
+                }
+            }
+        }
 
-        //private void OnLand(AnimationEvent animationEvent)
-        //{
-        //    if (animationEvent.animatorClipInfo.weight > 0.5f)
-        //    {
-        //        AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
-        //    }
-        //}
+        private void OnLand(AnimationEvent animationEvent)
+        {
+            if (animationEvent.animatorClipInfo.weight > 0.5f)
+            {
+                AudioSource.PlayClipAtPoint(LandingAudioClip, transform.TransformPoint(_controller.center), FootstepAudioVolume);
+            }
+        }
     }
 }
